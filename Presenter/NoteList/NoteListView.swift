bc051109c8f4bbe0d42b349d24d5d0ab833a7bec
DaiTran <daitran@vulcanlabs.co>
2023-08-08 00:17:04 +0700

    implement intial UI for NoteList

--- Presenter/NoteList/NoteListView.swift
+++ Presenter/NoteList/NoteListView.swift
@@ -5,4 +5,63 @@
 //  Created by Dai Tran on 07/08/2023.
 //
 
-import Foundation
+import Domain
+import SwiftUI
+
+struct NoteListView<T: NoteListViewModelType>: View {
+    @ObservedObject var viewModel: T
+    
+    public init(viewModel: T) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.notes, id: \.id) {
+                NoteListRow(note: $0)
+            }.toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        print("Edit button was tapped")
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct NoteListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListView(
+            viewModel: NoteListViewModel(
+                userNoteUseCase: UserNoteUseCaseImpl(
+                    userNoteRepository: UserNoteRepositoryMock()
+                ),
+                userInfo: UserInfo(userName: "Hello", notes: [])
+            )
+        )
+    }
+}
+
+private class UserNoteRepositoryMock: UserNotesRepository {
+    func createNote(userName: String, content: String) async throws -> [UserNote] {
+        []
+    }
+    
+    func updateNote(userName: String, note: UserNote) async throws -> [UserNote] {
+        []
+    }
+    
+    func fetchNotes(userName: String) async throws -> [UserNote] {
+        [
+            .init(id: "1", content: "Hello"),
+            .init(id: "2", content: "World"),
+        ]
+    }
+    
+    func deleteNote(userName: String, note: UserNote) async throws -> [UserNote] {
+        []
+    }
+}
--- Presenter/NoteList/NoteListViewModel.swift
+++ Presenter/NoteList/NoteListViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Dai Tran on 07/08/2023.
 //
 
+import Domain
 import Foundation
+
+public protocol NoteListViewModelType: ObservableObject {
+    var notes: [UserNote] { get set }
+}
+
+final class NoteListViewModel: NoteListViewModelType {
+    private let userNoteUseCase: UserNoteUseCase
+    private let userInfo: UserInfo
+    @Published var notes: [Domain.UserNote] = []
+    
+    init(userNoteUseCase: UserNoteUseCase, userInfo: UserInfo) {
+        self.userInfo = userInfo
+        self.userNoteUseCase = userNoteUseCase
+        
+        Task {
+            await fetchNotes()
+        }
+    }
+    
+    private func fetchNotes() async {
+        do {
+            let notes = try await userNoteUseCase.fetchNotes(userName: userInfo.userName)
+            await MainActor.run {
+                self.notes = notes
+            }
+        } catch {
+            
+        }
+    }
+}
