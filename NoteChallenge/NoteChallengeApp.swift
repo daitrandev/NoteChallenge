103ae8ac8e1d8ea4e0ae889d74d65766bbfaf09d
DaiTran <daitran@vulcanlabs.co>
2023-08-07 10:05:40 +0700

    Initial Commit

---
+++ NoteChallenge/NoteChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteChallengeApp.swift
+//  NoteChallenge
+//
+//  Created by Dai Tran on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
