fbd28b3e799010fcc390728151d41ad611293041
DaiTran <daitran@vulcanlabs.co>
2023-08-08 07:44:03 +0700

    add AddNoteView

---
+++ Presenter/AddNote/AddNoteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddNoteViewModel.swift
+//  Presenter
+//
+//  Created by Dai Tran on 08/08/2023.
+//
+
+import Foundation
---
+++ Presenter/Environments/UserInfoKey.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfoKey.swift
+//  Presenter
+//
+//  Created by Dai Tran on 08/08/2023.
+//
+
+import Foundation
