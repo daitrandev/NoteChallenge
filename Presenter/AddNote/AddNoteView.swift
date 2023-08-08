38615153556218edaea7dc9830dddbecb72c4827
DaiTran <daitran@vulcanlabs.co>
2023-08-08 13:37:07 +0700

    setup UI for AddNoteView

--- Presenter/AddNote/AddNoteView.swift
+++ Presenter/AddNote/AddNoteView.swift
@@ -5,4 +5,63 @@
 //  Created by Dai Tran on 08/08/2023.
 //
 
-import Foundation
+import Domain
+import SwiftUI
+
+public struct AddNoteView<T: AddNoteViewModelType>: View {
+    @ObservedObject var viewModel: T
+    @EnvironmentObject var loggedInUser: UserInfoEnv
+    
+    public init(viewModel: T) {
+        self.viewModel = viewModel
+    }
+    
+    public var body: some View {
+        VStack {
+            TextField("Note", text: $viewModel.note)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.black, lineWidth: 1)
+                )
+            Spacer()
+        }
+        .padding(.horizontal, 8)
+        .navigationBarTitleDisplayMode(.large)
+        .navigationTitle("Add Notes")
+        .toolbar {
+            Button("Done") {
+                Task {
+                    await viewModel.createNote(userName: loggedInUser.userName)
+                }
+            }
+        }
+    }
+}
+
+struct AddNote_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNoteView(viewModel: AddNoteViewModel(userNoteUseCase: UserNoteUseCaseImpl(userNoteRepository: UserNoteRepositoryMock())))
+    }
+}
+
+private class UserNoteRepositoryMock: UserNotesRepository {
+    func createNote(userName: String, content: String) async throws -> [UserNote] {
+        []
+    }
+    
+    func updateNote(userName: String, note: UserNote) async throws -> [UserNote] {
+        []
+    }
+    
+    func fetchNotes(userName: String) async throws -> [UserNote] {
+        [
+            .init(id: "1", content: "Hello"),
+            .init(id: "2", content: "World"),
+        ]
+    }
+    
+    func deleteNote(userName: String, note: UserNote) async throws -> [UserNote] {
+        []
+    }
+}
--- Presenter/AddNote/AddNoteViewModel.swift
+++ Presenter/AddNote/AddNoteViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by Dai Tran on 08/08/2023.
 //
 
+import Domain
 import Foundation
+
+public protocol AddNoteViewModelType: ObservableObject {
+    var note: String { get set }
+    func createNote(userName: String) async
+}
+
+public final class AddNoteViewModel: AddNoteViewModelType {
+    private let userNoteUseCase: UserNoteUseCase
+    
+    @Published public var note: String = ""
+    
+    public init(userNoteUseCase: UserNoteUseCase) {
+        self.userNoteUseCase = userNoteUseCase
+    }
+    
+    public func createNote(userName: String) async {
+        do {
+            _ = try await userNoteUseCase.createNote(userName: userName, noteContent: note)
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+    }
+}
