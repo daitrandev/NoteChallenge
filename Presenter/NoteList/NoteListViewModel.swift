b52530dda8465f2ba6e0b87954f5667b280d440f
DaiTran <daitran@vulcanlabs.co>
2023-08-08 13:22:20 +0700

    remove UserInfo from NoteListViewModel

--- Presenter/NoteList/NoteListViewModel.swift
+++ Presenter/NoteList/NoteListViewModel.swift
@@ -10,22 +10,20 @@ import Foundation
 
 public protocol NoteListViewModelType: ObservableObject {
     var notes: [UserNote] { get set }
-    func fetchNotes() async
+    func fetchNotes(userName: String) async
 }
 
 public final class NoteListViewModel: NoteListViewModelType {
     private let userNoteUseCase: UserNoteUseCase
-    private let userInfo: UserInfo
     @Published public var notes: [UserNote] = []
     
-    public init(userNoteUseCase: UserNoteUseCase, userInfo: UserInfo) {
+    public init(userNoteUseCase: UserNoteUseCase) {
         self.userNoteUseCase = userNoteUseCase
-        self.userInfo = userInfo
     }
     
-    public func fetchNotes() async {
+    public func fetchNotes(userName: String) async {
         do {
-            let notes = try await userNoteUseCase.fetchNotes(userName: userInfo.userName)
+            let notes = try await userNoteUseCase.fetchNotes(userName: userName)
             await MainActor.run {
                 self.notes = notes
             }
