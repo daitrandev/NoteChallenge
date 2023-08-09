e1a81d3bcbb4b1a905b9a140354282bbcfc9e8f2
DaiTran <daitran@vulcanlabs.co>
2023-08-09 16:55:50 +0700

    move all composing views into AppFactory

---
+++ NoteChallenge/AppFactory.swift
@@ -0,0 +1,79 @@
+//
+//  AppFactory.swift
+//  NoteChallenge
+//
+//  Created by Dai Tran on 8/9/23.
+//
+
+import SwiftUI
+import Data
+import Domain
+import Presenter
+import Foundation
+import FirebaseDatabase
+import FirebaseDatabaseSwift
+
+struct AppFactory {
+    static func makeFirebaseUserInfoService() -> UserInfoService {
+        FirebaseUserInfoService(databaseRef: Database.database(
+            url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+        )
+    }
+    
+    static func makeFirebaseUserNoteService() -> UserNoteService {
+        FirebaseUserNoteService(databaseRef: Database.database(
+            url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+        )
+    }
+    
+    static func makeFriendNotesView() -> some View {
+        let allNotesVM = AllNotesViewModel(
+            userInfoUseCase: UserInfoUseCaseImpl(
+                userInfoRepository: UserInfoRepositoryImpl(
+                    service: makeFirebaseUserInfoService()
+                )
+            )
+        )
+        return AllNotesView(viewModel: allNotesVM)
+    }
+    
+    static func makeAddNote(didAddNote: @escaping (UserNote?) -> Void) -> some View {
+        let addNoteVM = AddNoteViewModel(
+            userNoteUseCase: UserNoteUseCaseImpl(
+                userNoteRepository: UserNotesRepositoryImpl(
+                    userNoteService: FirebaseUserNoteService(databaseRef: Database.database(url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference())
+                )
+            )
+        )
+        return AddNoteView(viewModel: addNoteVM, didAddNote: didAddNote)
+    }
+    
+    static func makeLoggedView() -> some View {
+        let noteListVM = NoteListViewModel(
+            userNoteUseCase: UserNoteUseCaseImpl(
+                userNoteRepository: UserNotesRepositoryImpl(
+                    userNoteService: makeFirebaseUserNoteService()
+                )
+            )
+        )
+        
+        return NoteListView(
+            viewModel: noteListVM,
+            addNoteView: { makeAddNote(didAddNote: { _ in noteListVM.viewState = .noteList }) }
+        )
+    }
+    
+    static func makeLogginView() -> some View {
+        let loginVM = LoginViewModel(
+            userInfoUseCase: UserInfoUseCaseImpl(
+                userInfoRepository: UserInfoRepositoryImpl(
+                    service: makeFirebaseUserInfoService()
+                )
+            )
+        )
+        return LoginView(
+            viewModel: loginVM,
+            loggedInView: makeLoggedView
+        )
+    }
+}
--- NoteChallenge/NoteChallengeApp.swift
+++ NoteChallenge/NoteChallengeApp.swift
@@ -19,69 +19,15 @@ struct NoteChallengeApp: App {
     var body: some Scene {
         WindowGroup {
             TabView {
-                makeLogginView()
+                AppFactory.makeLogginView()
                     .tabItem {
                         Label("Personal", systemImage: "person")
                     }
-                makeFriendNotesView()
+                AppFactory.makeFriendNotesView()
                     .tabItem {
                         Label("All Notes", systemImage: "line.horizontal.3")
                     }
             }
         }
-    }
-    
-    private func makeFriendNotesView() -> some View {
-        let friendNoteVM = AllNotesViewModel(
-            userInfoUseCase: UserInfoUseCaseImpl(
-                userInfoRepository: UserInfoRepositoryImpl(
-                    service: FirebaseUserInfoService(databaseRef: Database.database(url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference())
-                    )
-                )
-            )
-        return AllNotesView(viewModel: friendNoteVM)
-    }
-    
-    private func makeLogginView() -> some View {
-        let loginVM = LoginViewModel(
-            userInfoUseCase: UserInfoUseCaseImpl(
-                userInfoRepository: UserInfoRepositoryImpl(
-                    service: FirebaseUserInfoService(databaseRef: Database.database(url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference())
-                )
-            )
-        )
-        return LoginView(
-            viewModel: loginVM,
-            loggedInView: makeLoggedView
-        )
-    }
-    
-    private func makeAddNote(didAddNote: @escaping (UserNote?) -> Void) -> some View {
-        let addNoteVM = AddNoteViewModel(
-            userNoteUseCase: UserNoteUseCaseImpl(
-                userNoteRepository: UserNotesRepositoryImpl(
-                    userNoteService: FirebaseUserNoteService(databaseRef: Database.database(url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference())
-                )
-            )
-        )
-        return AddNoteView(viewModel: addNoteVM, didAddNote: didAddNote)
-    }
-    
-    private func makeLoggedView() -> some View {
-        let noteListVM = NoteListViewModel(
-            userNoteUseCase: UserNoteUseCaseImpl(
-                userNoteRepository: UserNotesRepositoryImpl(
-                    userNoteService: FirebaseUserNoteService(
-                        databaseRef: Database.database(url: "https://notechallenge-52479-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
-                    )
-                )
-            )
-        )
-        
-        
-        return NoteListView(
-            viewModel: noteListVM,
-            addNoteView: { makeAddNote(didAddNote: { _ in noteListVM.viewState = .noteList }) }
-        )
     }
 }
