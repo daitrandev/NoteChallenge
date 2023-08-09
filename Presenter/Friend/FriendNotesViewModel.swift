4225ae488ee18e95136eea0d3926152679e2c039
DaiTran <daitran@vulcanlabs.co>
2023-08-09 08:32:18 +0700

    setup user name input observation

--- Presenter/Friend/FriendNotesViewModel.swift
+++ Presenter/Friend/FriendNotesViewModel.swift
@@ -25,15 +25,7 @@ public final class FriendNotesViewModel: FriendNotesViewModelType {
     public init(userNoteUseCase: UserNoteUseCase) {
         self.userNoteUseCase = userNoteUseCase
         
-        $username
-            .debounce(for: .seconds(1), scheduler: RunLoop.main)
-            .drop(while: { $0.isEmpty })
-            .sink(receiveValue: { [fetchUserNotes] value in
-                Task.detached(priority: .userInitiated) {
-                    await fetchUserNotes()
-                }
-            })
-            .store(in: &subscriptions)
+        setupObservation()
     }
     
     public func fetchUserNotes() async {
@@ -45,5 +37,17 @@ public final class FriendNotesViewModel: FriendNotesViewModelType {
         } catch {
             debugPrint(error.localizedDescription)
         }
+    }
+    
+    private func setupObservation() {
+        $username
+            .debounce(for: .seconds(1), scheduler: RunLoop.main)
+            .drop(while: { $0.isEmpty })
+            .sink(receiveValue: { [fetchUserNotes] value in
+                Task.detached(priority: .userInitiated) {
+                    await fetchUserNotes()
+                }
+            })
+            .store(in: &subscriptions)
     }
 }
